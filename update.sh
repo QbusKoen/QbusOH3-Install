9bf07da5e02f5e7a123fa7b6df879e30a673150e
Koen Schockaert <54985218+QbusKoen@users.noreply.github.com>
2020-12-05 10:23:42 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 sudo rm -R ~/QbusOH3    # Verwijderen van oude files
 git clone https://github.com/QbusKoen/QbusOH3 ~/QbusOH3    # Downloaden van nieuwe bestanden
 sudo rm /usr/share/openhab/addons/org.openhab.binding.qbus-*      # Verwijderen van alle Qbus-JAR files
-sudo cp ~/QbusOpenHab/JAR/org.openhab.binding.qbus-3.0.0-SNAPSHOT.jar /usr/share/openhab/addons/    # Kopiëren van huidige JAR naar openHAB
+sudo cp ~/QbusOH3/JAR/org.openhab.binding.qbus-3.0.0-SNAPSHOT.jar /usr/share/openhab/addons/    # Kopiëren van huidige JAR naar openHAB
 sudo systemctl stop qbusclient.service    # oude client stoppen
 sudo systemctl stop qbusserver.service    # oude server stoppen
 sudo systemctl start qbusserver.service   # nieuwe server starten
